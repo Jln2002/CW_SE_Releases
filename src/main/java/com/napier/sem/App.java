8736e607e50eaa6371ccbc9b8b4a84904ac8f10e
40598699 <40598699@live.napier.ac.uk>
2023-02-16 10:38:30 +0000

    fixed something

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,6 @@ package com.napier.sem;
 
 public class App{
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
     }
 }
