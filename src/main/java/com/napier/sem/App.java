10b4c2bd725b5c7393fe95b2f73eeea1d0fc9201
jln2002 <40597506@live.napier.ac.uk>
2023-02-23 10:43:19 +0000

    changed to mysql

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -26,7 +26,7 @@ public class App
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(0);
+                Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
